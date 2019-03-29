a8d0b9469a01a714e6ba2767bb301798d6a73af9
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-03-29 16:20:59 -0600

    test load_multicam 1

--- io/load_multicam.py
+++ io/load_multicam.py
@@ -1,3 +1,13 @@
 from get_ref import *
+import os
 
-print(get_ref('easy_pose'))
+def load_multicam(dataset, group_name, section_number, optional_frames=None):
+    home_path = get_ref(dataset)
+
+    depth = 'depthRender'
+    classes = 'groundtruth'
+    file_format = 'mayaProject.{:06d}.png'
+    base_path = os.path.join(home_path, group_name, str(section_number))
+    print(base_path)
+
+load_multicam('easy_pose', 'train', 3, "jim")
