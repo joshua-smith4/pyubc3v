f8235f5142a30661cd181b07486ce7c10c5fcbfb
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-03-29 16:53:56 -0600

    got camera names 6

--- io/load_multicam.py
+++ io/load_multicam.py
@@ -21,4 +21,7 @@ def load_multicam(dataset, group_name, section_number, optional_frames=None):
     camera_names = groundtruth_file['cameras']
     print('Identified {:d} cameras'.format(np.size(camera_names)))
 
+    image_names = os.path.join(base_path, 'images', depth, str(camera_names[0]), '*.png')
+    print(image_names)
+
 load_multicam('easy_pose', 'train', 3, "jim")
