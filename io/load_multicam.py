cf7eb5a0f481109d5732751f468a75202ed538bb
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-03-29 16:32:57 -0600

    got camera names

--- io/load_multicam.py
+++ io/load_multicam.py
@@ -1,13 +1,23 @@
 from get_ref import *
 import os
+from scipy.io import loadmat
 
 def load_multicam(dataset, group_name, section_number, optional_frames=None):
     home_path = get_ref(dataset)
 
     depth = 'depthRender'
     classes = 'groundtruth'
-    file_format = 'mayaProject.{:06d}.png'
+    file_format = 'mayaProject.{idx:06d}.png'
     base_path = os.path.join(home_path, group_name, str(section_number))
-    print(base_path)
+
+    depth_path = os.path.join(base_path, 'images', depth, '{camera_name}', file_format)
+    # print(depth_path.format(camera_name='Cam1', idx=25))
+    class_path = os.path.join(base_path, 'images', classes, '{camera_name}', file_format)
+
+    print('Loading groundtruth file {:d} ...'.format(section_number))
+    groundtruth_file = loadmat(os.path.join(base_path, 'groundtruth.mat'))
+
+    camera_names = groundtruth_file.keys()
+    print('Identified cameras: {}'.format(camera_names))
 
 load_multicam('easy_pose', 'train', 3, "jim")
