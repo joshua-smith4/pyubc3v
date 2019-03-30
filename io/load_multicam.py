a614bdbb92f2b917a53fe088023e6554ee84f760
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-03-29 19:45:03 -0600

    wip

--- io/load_multicam.py
+++ io/load_multicam.py
@@ -66,7 +66,7 @@ def load_multicam(dataset, group_name, section_number, optional_frames=None):
     print(len(cameras[0][0][0][0][0][0][0][0]["rotate"]))
     print(np.squeeze(cameras[0][0][0][0][0][0][0][0]["rotate"]).shape)
     print(cameras[0][0][0][0][0][0][0][0]["rotate"].dtype)
-    print(cameras[0][0][0][0][0][0][0][0]["rotate"])
+    print(np.squeeze(cameras[0][0][0][0][0][0][0][0]["rotate"]))
     image_folder_path = os.path.join(base_path, 'images', depth, str(camera_names[0]))
     image_names = [f for f in os.listdir(image_folder_path) if f.endswith('.png')]
     print('\tExpecting {:d} frames'.format(len(image_names)))
